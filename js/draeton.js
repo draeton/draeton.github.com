105b4c58bc09f50240f06d65b80cfde2af3486cd
Matthew Cobbs <draeton@gmail.com>
2011-12-04 10:02:20 -0500

    adding google cse

---
+++ js/draeton.js
@@ -0,0 +1,18 @@
+// ## draeton.github.com
+// 
+// Site code for my personal site
+// [http://draeton.github.com/draeton.github.com](http://draeton.github.com/draeton.github.com)
+//
+// Copyright 2011, Matthew Cobbs  
+// Licensed under the MIT license.
+//
+/*global jQuery*/
+(function (window, $) {
+    
+    $(function () {
+        $("#cse-modal").modal({
+            keyboard: true
+        });
+    });
+    
+})(window, jQuery);
